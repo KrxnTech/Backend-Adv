69ec77000fe69e2abd9156ff5b45fb20f7ed662b
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-02-21 19:34:14 +0530

    Making User Model + JWT + bcrypt : Auth System and some Extra notes

---
+++ src/models/user.model.js
@@ -0,0 +1,233 @@
+import mongoose, { Schema } from "mongoose";
+
+/* Working on PassWord Encryption : Using MongoDB Hooks ex - PRE */
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import { useCallback } from "react";
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String, // 3RD PARTY : URL
+        required: true,
+    },
+    converImage: {
+        type: String, // // 3RD PARTY : URL
+    },
+
+    /* REMEMBER THIS REFRENCE SYNTAC */
+    WatchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, "PassWord is Required Can't Leave it Empty !!!"],
+    },
+    refreshToken: {
+        type: String,
+    }
+
+}, { timestamps: true })
+
+/* Working on Hooks : PRE - Middle Ware using Next */
+UserSchema.pre("save", async function (next) {
+    /* next : Pass Further this flag  */
+    if (!this.isModified("password")) {
+        return next()
+    }
+    this.password = bcrypt.hash(this.password, 10) /* 10 is Round */
+    next()
+})
+
+/* Problem :  On Model Schema Change it will run again and again */
+
+UserSchema.methods.isPassWordCorrect = async function (password) {
+    /*  Chechking PassWord : Before Saving */
+    return await bcrypt.compare(password, this.password) /* Crypto Graphy : Computatin power takes time */
+
+    /* Comparing both the Password : String Vs Encrypted */
+}
+
+/* JWT TOKEN */
+UserSchema.methods.generateAccessToken = function () {
+    // It can Gen Token 
+    jwt.sign({
+        _id: this._id,  // From MongoDB 
+
+        // From Our Side 
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    },
+
+        process.env.ACCESS_TOKEN_SECRET,
+        // Expiry in Object 
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+/* JWT TOKEN */
+UserSchema.methods.generateRefreshToken = function () {
+    jwt.sign({
+        _id: this._id
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+
+export const User = mongoose.model("User", UserSchema)
+
+/*
+
+Bcrypt kind of Authentication Zone 
+
+Sign Up: Create a new Acc || You use this when a user is new and wants to register || I dont exist i want to create 
+What is takes -> Name + Email + Password 
+
+
+Log In: Enter an Existing Account || User already has an account.Now they are proving who thet are || I already Exist let me in
+You Enter email + password
+Backend Check :
+    Does User Exist ?
+    Does Hashed Password Match ?
+    If correct - Gen JWT / Session 
+    Send token or Cookie 
+What is takes -> Email + Password
+
+
+Sign In: This is Basically the same as Log In 
+Google say: Sign In
+Many Diff Company say: Log In 
+
+
+Log Out: End the Session 
+User is currently authenticated and want's to exit 
+Backend work depend's on auth method 
+Kick me out(Session end)
+
+Want to Know about : Token || JWT || Cookies || Session || Bcrypt || Flow 
+
+Big Picture : How do I remember this user in future Req ? 
+That's where this upper sitting mf's comes on the stage 
+
+
+
+*/
+
+
+/*
+    What is Bcrypt ? 
+    ---------------
+- A Password Hashing Library 
+- You never store plan password in DB : if you do that you are nothing but Just peace of shit ( Lawda Dev )
+- What is Does 
+    instead of storing : password = "krishna123" : Nonsense 
+    make it like this : password = "gHsjkKjdhskjdh"
+
+    What is Hasing ? 
+- One Way Transformation 
+- Cannot Reverse back to original password 
+
+   // HOW IT WORKS // 
+   Frontend Side : JSON Sign Up Flow 
+   {
+        "email": "user@gmail.com",
+        "password": "123456"
+    }
+    Backend : bcrypt.hash(password)
+    Store hashed password in DB 
+
+    DataBase working 
+    ----------------
+    CODE : 
+    {
+        email: "user@gmail.com",
+        password: "$2b$10$..."
+    }
+
+    During Login 
+        - User send password 
+        - Backend uses bcrypt.compare()
+        - if Matches : Success 
+
+
+    */
+/*
+    What is Session ? 
+    ----------------
+    It is Basically used for the check that user is authenticated or not 
+    It is a old school method 
+    
+    How it Work's 
+    -------------
+    When user log's in
+    Backend creates a session object in server database 
+    Server gen a random session Id 
+    Send that Id to browser ( inside cookies it send )
+
+*/
+
+
+/*
+
+    What is Cookie ? 
+    - It is used to indentify user 
+    - We go to any website and fill out email and password ( Remember me buttont : click )
+      Browser store's our info but that ok button req goes to server and it gen a SessionID and that will store in cookie 
+
+    // Cookie Store what extra // 
+    - Visited pages ( last previus )
+    - Product clicking 
+    - Depend's on website action .... 
+
+
+    Cookie store SessionId 
+    it can be anymore ... 
+
+    Cookie store's in our browser Always 
+    Size : 4KB 
+
+    If we come's in again : Server identify with our old sessionId and we get the web site access 
+    
+
+*/
+
+
+/*
+    What is JWT ? 
+    ------------
+ The Full Form of JWT is Json Web Token .... 
+
+
+*/
